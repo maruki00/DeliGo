bacd2d74f65e8b7d03f5c54188101c16984e98d8
maruki00 <aitbaha48@gmail.com>
2025-03-07 14:16:22 +0000

    --

--- internal/product/app/routes/prouduct.go
+++ internal/product/app/routes/prouduct.go
@@ -1,10 +1,10 @@
 package routes
 
 import (
-	"delivery/internal/product/userGetway/controllers"
-
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	"delivery/internal/product/userGetway/controllers"
 )
 
 var ProductRouter = func(router *gin.Engine, db *gorm.DB) {
@@ -12,10 +12,24 @@ var ProductRouter = func(router *gin.Engine, db *gorm.DB) {
 	controller := controllers.NewProductController(db)
 
 	prefix := router.Group("/api/product")
-	_ = prefix.POST("/insert", controller.Insert)
-	_ = prefix.POST("/update", controller.Update)
+	_ = prefix.POST  ("/insert", controller.Insert)
+	_ = prefix.POST  ("/update", controller.Update)
 	_ = prefix.DELETE("/delete", controller.Delete)
-	_ = prefix.POST("/search", controller.Search)
-	_ = prefix.POST("/get", controller.GetProduct)
-	_ = prefix.POST("/multiple", controller.MultipleProducts)
+	_ = prefix.POST  ("/search", controller.Search)
+	_ = prefix.POST  ("/get", controller.GetProduct)
+	_ = prefix.POST  ("/multiple", controller.MultipleProducts)
 }
+
+
+post:"/v0/product/insert",
+body: "*"
+post:"/v0/product/update",
+body: "*"
+delete:"/v0/product/delete",
+body: "*"
+post:"/v0/product/search",
+body: "*"
+post:"/v0/product/get",
+body: "*"
+post:"/v0/product/multiple",
+body: "*"
