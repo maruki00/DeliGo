745ea0f1bbc3476d16ac893af1715c881b15ba44
maruki00 <aitbaha48@gmail.com>
2025-03-09 03:08:24 +0000

    commit

--- internal/user/app/app.go
+++ internal/user/app/app.go
@@ -3,19 +3,18 @@ package app
 import (
 	"context"
 	"delivery/cmd/user/configs"
-	"delivery/internal/user/app/services"
 	"delivery/internal/user/domain/contracts"
 	"delivery/internal/user/infra/repositories"
-	"delivery/internal/user/userGateWay/adapters/presenters"
+	pkgPostgres "delivery/pkg/postgres"
+	"log/slog"
 
-	"github.com/go-playground/validator"
 	"github.com/rabbitmq/amqp091-go"
 )
 
 type App struct {
-	cgf  configs.Config
-	db   pkgPostgres.PGHandler
-	Repo contracts.IAuthRepository
+	cgf       configs.Config
+	DBHandler pkgPostgres.PGHandler
+	Repo      contracts.IAuthRepository
 }
 
 func InitApp(cfg *configs.Config) (*App, error) {
@@ -24,16 +23,27 @@ func InitApp(cfg *configs.Config) (*App, error) {
 	if err != nil {
 		return nil, err
 	}
-	outPort := &presenters.JsonAuthPresenter{}
-	inPort := &services.AuthService{}
+	// outPort := &presenters.JsonAuthPresenter{}
+	// inPort := &services.AuthService{}
 	return &App{
-		Repo:       repo,
-		InputPort:  inPort,
-		OutputPort: outPort,
-		Validate:   validator.New(),
+		Repo: repo,
+		// InputPort:  inPort,
+		// OutputPort: outPort,
+		// Validate:   validator.New(),
 	}, nil
 }
 
 func (a *App) Worker(ctx context.Context, deivery <-chan amqp091.Delivery) {
 
+	for {
+		select {
+		case <-ctx.Done():
+			slog.Info("Shuting Down the client.")
+			break
+		default:
+			slog.Info("default interception ....")
+		}
+	}
+	forever := struct{}{}
+	<-forever
 }
