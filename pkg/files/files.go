75d6569f77839adf43be683df668caceab864cfe
maruki00 <aitbaha48@gmail.com>
2025-03-23 23:47:06 +0000

    commit

---
+++ pkg/files/files.go
@@ -0,0 +1,35 @@
+package pkgFiles
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func MoveFile(sourcePath, destPath string) error {
+
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %v", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open dest file: %v", err)
+	}
+	defer outputFile.Close()
+
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return fmt.Errorf("Couldn't copy to dest from source: %v", err)
+	}
+
+	inputFile.Close()
+
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't remove source file: %v", err)
+	}
+	return nil
+}
