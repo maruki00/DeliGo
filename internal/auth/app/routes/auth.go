f70812a0d5baa8fbedecba81e0bf36227ac9e9c2
maruki00 <aitbaha48@gmail.com>
2025-03-23 23:52:44 +0000

    commit

--- internal/auth/app/routes/auth.go
+++ internal/auth/app/routes/auth.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"delivery/internal/auth/app"
-	"delivery/internal/auth/userGateWay/controllers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,12 +9,12 @@ import (
 var AuthRouter = func(router *gin.Engine, app *app.App) {
 
 	// repo := auth_infra_repository.NewAuthRepository(shared_configs.GetConfig())
-	controller := controllers.NewAuthController(app)
+	// controller := controllers.NewAuthController(app)
 
-	prefix := router.Group("/v1/auth")
-	_ = prefix.POST("/login", controller.Login)
-	_ = prefix.POST("/register", controller.Register)
-	_ = prefix.PATCH("/2f-confirm", controller.TwoFactoryConfirm)
-	_ = prefix.PATCH("/logout", controller.Logout)
+	// prefix := router.Group("/v1/auth")
+	// _ = prefix.POST("/login", controller.Login)
+	// _ = prefix.POST("/register", controller.Register)
+	// _ = prefix.PATCH("/2f-confirm", controller.TwoFactoryConfirm)
+	// _ = prefix.PATCH("/logout", controller.Logout)
 
 }
