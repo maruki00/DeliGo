4861438ab8cbcf779b57418acbfb6cd3683da8e9
maruki00 <aitbaha48@gmail.com>
2025-03-23 23:48:22 +0000

    commit

---
+++ pkg/utils/id.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/oklog/ulid"
+)
+
+func Id() string {
+	entropy := rand.New(rand.NewSource(time.Now().UnixNano()))
+	ms := ulid.Timestamp(time.Now())
+	ret, err := ulid.New(ms, entropy)
+	if err != nil {
+		return ""
+	}
+	return ret.String()
+}
