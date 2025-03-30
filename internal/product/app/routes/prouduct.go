c863334e4a14be0614b3134acddeea7a5fb53e3e
maruki00 <aitbaha48@gmail.com>
2025-03-30 16:45:45 +0000

    commit

--- internal/product/app/routes/prouduct.go
+++ internal/product/app/routes/prouduct.go
@@ -12,24 +12,23 @@ var ProductRouter = func(router *gin.Engine, db *gorm.DB) {
 	controller := controllers.NewProductController(db)
 
 	prefix := router.Group("/api/product")
-	_ = prefix.POST  ("/insert", controller.Insert)
-	_ = prefix.POST  ("/update", controller.Update)
+	_ = prefix.POST("/insert", controller.Insert)
+	_ = prefix.POST("/update", controller.Update)
 	_ = prefix.DELETE("/delete", controller.Delete)
-	_ = prefix.POST  ("/search", controller.Search)
-	_ = prefix.POST  ("/get", controller.GetProduct)
-	_ = prefix.POST  ("/multiple", controller.MultipleProducts)
+	_ = prefix.POST("/search", controller.Search)
+	_ = prefix.POST("/get", controller.GetProduct)
+	_ = prefix.POST("/multiple", controller.MultipleProducts)
 }
 
-
-post:"/v0/product/insert",
-body: "*"
-post:"/v0/product/update",
-body: "*"
-delete:"/v0/product/delete",
-body: "*"
-post:"/v0/product/search",
-body: "*"
-post:"/v0/product/get",
-body: "*"
-post:"/v0/product/multiple",
-body: "*"
+// post:"/v0/product/insert",
+// body: "*"
+// post:"/v0/product/update",
+// body: "*"
+// delete:"/v0/product/delete",
+// body: "*"
+// post:"/v0/product/search",
+// body: "*"
+// post:"/v0/product/get",
+// body: "*"
+// post:"/v0/product/multiple",
+// body: "*"
