f7cef60418e99604d75d309ed2105c48edd66da8
maruki00 <aitbaha48@gmail.com>
2025-03-31 08:20:13 +0000

    commit

--- internal/user/app/grpc/services/ProfileService.go
+++ internal/user/app/grpc/services/ProfileService.go
@@ -1,4 +1,13 @@
 package grpc_services
 
+import "delivery/internal/user/infrastructure/repositories"
+
 type ProfileService struct {
+	repository repositories.UserReposiroty
+}
+
+func NewProfileService(repository repositories.UserReposiroty) *ProfileService {
+	return &ProfileService{
+		repository: repository,
+	}
 }
--- internal/user/infra/repositories/userRepository.go
+++ internal/user/infra/repositories/userRepository.go
@@ -1,9 +1,88 @@
 package repositories
 
 import (
+	"context"
+	"delivery/internal/user/domain/entities"
+	pkgPostgres "delivery/pkg/postgres"
 	"sync"
 )
 
 type UserRepository struct {
-	sync.Mutex
+	sync.RWMutex
+	db pkgPostgres.PGHandler
+}
+
+func NewUserRepository(db pkgPostgres.PGHandler) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (ur *UserRepository) Create(ctx context.Context,entity entities.UserEntity) (entities.UserEntity, error){
+	sql := `INSERT INTO users(email, password, role) VALUES($1, $2, $3) RETURNING id`
+	tx, err := ur.db.GetDB().Begin()
+	if err != nil {
+		return nil, err
+	}
+	var lastInsertID string
+	stmnt, err := tx.QueryRow(sql, entity.GetEmail(), entity.GetPassword(), entity.GetRole()).Scan(&lastInsertID)
+	if err != nil {
+		return nil, err
+	}
+	defer stmnt.Close()
+	if err := tx.Commit(); err!= nil {
+		tx.Rollback()
+		return nil, err
+	}
+	entity.SetID(lastInsertID)
+	return entity, nil
+}
+
+func (ur *UserRepository) Delete(ctx context.Context, id string) (bool, error){
+	sql := `DELETE FROM users WHERE id = $1`
+	tx, err := ur.db.GetDB().Begin()
+	if err != nil {
+		return false, err
+	}
+	var lastInsertID string
+	stmnt, err := tx.QueryRow(sql, id)
+	if err != nil {
+		return false, err
+	}
+	defer stmnt.Close()
+	if err := tx.Commit(); err!= nil {
+		tx.Rollback()
+		return false, err
+	}
+	return true, nil
+}
+
+func (ur *UserRepository) Update(ctx context.Context, entity entities.UserEntity) (entities.UserEntity, error){
+
+	sql := `UPDATE users SET email=$1, role=$2, updated_at = now() WHERE id=$3`
+	tx, err := ur.db.GetDB().Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	stmnt, err := tx.QueryRow(sql, entity.GetEmail(), entity.GetRole(), entity.GetID)
+	if err != nil {
+		return nil, err
+	}
+	defer stmnt.Close()
+	if err := tx.Commit(); err!= nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	return entity, nil
+}
+func (ur *UserRepository) GetOne(ctx context.Context, any) (any, error){
+	return nil, nil
+}
+func (ur *UserRepository) GetMany(ctx context.Context, any)(any, error){
+	return nil, nil
+}
+func (ur *UserRepository) Search(ctx context.Context, any) (any, error){
+	return nil, nil
 }
