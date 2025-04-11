7d571c375948a403d16915bd6ff426ff621c565d
maruki00 <aitbaha48@gmail.com>
2025-04-11 23:51:00 +0100

    commit

--- Makefile
+++ Makefile
@@ -2,27 +2,31 @@
 
 
 
+# protoc -Iproto --go_out=pb --go-grpc_out=pb proto/profile.proto
 
 
-
-
+# cd proto && protoc -Iproto --go_out=../protogen/golang --go_opt=paths=source_relative \
+# --go-grpc_out=../protogen/golang --go-grpc_opt=paths=source_relative \
+# --grpc-gateway_out=../protogen/golang --grpc-gateway_opt paths=source_relative \
+# --grpc-gateway_opt generate_unbound_methods=true \
+# ./user/*.proto
 
 user:
-	cd proto && protoc --go_out=../protogen/golang --go_opt=paths=source_relative \
+	cd proto && protoc -I. -I./user --go_out=../protogen/golang --go_opt=paths=source_relative \
 	--go-grpc_out=../protogen/golang --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=../protogen/golang --grpc-gateway_opt paths=source_relative \
 	--grpc-gateway_opt generate_unbound_methods=true \
 	./user/*.proto
 
 profile:
-	cd proto && protoc --go_out=../protogen/golang --go_opt=paths=source_relative \
+	cd proto && protoc -Iproto --go_out=../protogen/golang --go_opt=paths=source_relative \
 	--go-grpc_out=../protogen/golang --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=../protogen/golang --grpc-gateway_opt paths=source_relative \
 	--grpc-gateway_opt generate_unbound_methods=true \
 	./profile/*.proto
 
 auth:
-	cd proto && protoc --go_out=../protogen/golang --go_opt=paths=source_relative \
+	cd proto && protoc -Iproto --go_out=../protogen/golang --go_opt=paths=source_relative \
 	--go-grpc_out=../protogen/golang --go-grpc_opt=paths=source_relative \
 	--grpc-gateway_out=../protogen/golang --grpc-gateway_opt paths=source_relative \
 	--grpc-gateway_opt generate_unbound_methods=true \
--- internal/user/app/grpc/services/UserService.go
+++ internal/user/app/grpc/services/UserService.go
@@ -1,4 +1,56 @@
 package grpc_services
 
+import (
+	"context"
+	"delivery/internal/user/domain/entities"
+	user_grpc "delivery/internal/user/infra/grpc/user"
+	"delivery/internal/user/infra/models"
+	"delivery/internal/user/infra/repositories"
+)
+
 type UserService struct {
+	user_grpc.UnimplementedUserServiceServer
+	userRepo *repositories.UserRepository
+}
+
+var _ entities.UserEntity = (*models.User)(nil)
+
+func (us *UserService) Create(ctx context.Context, createUserRequest *user_grpc.CreateUserRequest) (*user_grpc.UserResponse, error) {
+	res, err := us.userRepo.Create(ctx, nil)
+	if err != nil {
+		return &user_grpc.UserResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	if err != nil {
+		return &user_grpc.UserResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+	// anypb.UnmarshalTo(res, anyRes)
+	return &user_grpc.UserResponse{
+		Code:    200,
+		Message: "success",
+		Result:  []byte(res),
+	}, nil
+}
+func (us *UserService) Delete(context.Context, *user_grpc.DeleteUserRequest) (*user_grpc.UserResponse, error) {
+	return nil, nil
+}
+func (us *UserService) GetMany(context.Context, *user_grpc.EmptyUserResponse) (*user_grpc.UserResponse, error) {
+	return nil, nil
+}
+func (us *UserService) GetOne(context.Context, *user_grpc.EmptyUserResponse) (*user_grpc.UserResponse, error) {
+	return nil, nil
+}
+func (us *UserService) Search(context.Context, *user_grpc.EmptyUserResponse) (*user_grpc.UserResponse, error) {
+	return nil, nil
+}
+func (us *UserService) Update(context.Context, *user_grpc.UpdateUserRequest) (*user_grpc.UserResponse, error) {
+	return nil, nil
 }
