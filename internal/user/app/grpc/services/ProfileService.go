9017f142e711b1bf9c079db1832bb2eeffd78354
maruki00 <aitbaha48@gmail.com>
2025-04-17 23:38:21 +0100

    commit

--- internal/user/app/grpc/services/ProfileService.go
+++ internal/user/app/grpc/services/ProfileService.go
@@ -6,8 +6,6 @@ import (
 	"delivery/internal/user/infra/models"
 	"delivery/internal/user/infra/repositories"
 
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -59,18 +57,153 @@ func (_this *ProfileService) Create(ctx context.Context, in *profile_grpc.Create
 	}, nil
 
 }
-func (_this *ProfileService) Delete(context.Context, *profile_grpc.DeleteProfileRequest) (*profile_grpc.ProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
+
+func (_this *ProfileService) Delete(ctx context.Context, in *profile_grpc.DeleteProfileRequest) (*profile_grpc.ProfileResponse, error) {
+	isDone, err := _this.repository.Delete(ctx, in.GetID())
+	if err != nil || !isDone {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  nil,
+	}, nil
 }
-func (_this *ProfileService) Update(context.Context, *profile_grpc.UpdateProfileRequest) (*profile_grpc.ProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Update not implemented")
+func (_this *ProfileService) Update(ctx context.Context, in *profile_grpc.UpdateProfileRequest) (*profile_grpc.ProfileResponse, error) {
+	res, err := _this.repository.Update(ctx, &models.Profile{
+		ID:       in.ID,
+		FullName: in.Fields["full_name"],
+		Avatar:   in.Fields["avatar"],
+		Bio:      in.Fields["bio"],
+	})
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	stuctRes, err := structpb.NewValue(map[string]any{
+		"id":        res.GetID(),
+		"Full_name": res.GetFullName(),
+		"bio":       res.GetBio(),
+		"avatr":     res.GetAvatar(),
+	})
+
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  []*structpb.Value{stuctRes},
+	}, nil
 }
-func (_this *ProfileService) GetOne(context.Context, *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetOne not implemented")
+func (_this *ProfileService) GetOne(ctx context.Context, in *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
+	res, err := _this.repository.GetOne(ctx, "in.Page")
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	stuctRes, err := structpb.NewValue(map[string]any{
+		"id":        res.GetID(),
+		"Full_name": res.GetFullName(),
+		"bio":       res.GetBio(),
+		"avatr":     res.GetAvatar(),
+	})
+
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  []*structpb.Value{stuctRes},
+	}, nil
 }
-func (_this *ProfileService) GetMany(context.Context, *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetMany not implemented")
+func (_this *ProfileService) GetMany(ctx context.Context, in *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
+	res, err := _this.repository.GetMany(ctx, int(in.Offset), int(in.Page))
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	items := make([]*structpb.Value, len(res))
+	i := 0
+	for _, item := range res {
+		itm, err := structpb.NewValue(map[string]any{
+			"id":        item.GetID(),
+			"Full_name": item.GetFullName(),
+			"bio":       item.GetBio(),
+			"avatr":     item.GetAvatar(),
+		})
+		if err != nil {
+			continue
+		}
+
+		items[i] = itm
+		i++
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  items[:i],
+	}, nil
 }
-func (_this *ProfileService) Search(context.Context, *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Search not implemented")
+func (_this *ProfileService) Search(ctx context.Context, in *profile_grpc.EmptyProfileResponse) (*profile_grpc.ProfileResponse, error) {
+	res, err := _this.repository.Search(ctx, "in.GetPage()", int(in.Offset), int(in.Page))
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	items := make([]*structpb.Value, len(res))
+	i := 0
+	for _, item := range res {
+		itm, err := structpb.NewValue(map[string]any{
+			"id":        item.GetID(),
+			"Full_name": item.GetFullName(),
+			"bio":       item.GetBio(),
+			"avatr":     item.GetAvatar(),
+		})
+		if err != nil {
+			continue
+		}
+
+		items[i] = itm
+		i++
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  items[:i],
+	}, nil
 }
