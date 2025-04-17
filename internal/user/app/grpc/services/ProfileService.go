c25adb1255c029148a351b9cc5a59c13b3622b5a
maruki00 <aitbaha48@gmail.com>
2025-04-17 23:27:18 +0100

    commit

--- internal/user/app/grpc/services/ProfileService.go
+++ internal/user/app/grpc/services/ProfileService.go
@@ -3,10 +3,12 @@ package grpc_services
 import (
 	"context"
 	profile_grpc "delivery/internal/user/infra/grpc/profile"
+	"delivery/internal/user/infra/models"
 	"delivery/internal/user/infra/repositories"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type ProfileService struct {
@@ -20,9 +22,42 @@ func NewProfileService(repository *repositories.ProfileRepository) *ProfileServi
 	}
 }
 
-func (_this *ProfileService) Create(context.Context, *profile_grpc.CreateProfileRequest) (*profile_grpc.ProfileResponse, error) {
+func (_this *ProfileService) Create(ctx context.Context, in *profile_grpc.CreateProfileRequest) (*profile_grpc.ProfileResponse, error) {
+	res, err := _this.repository.Create(ctx, &models.Profile{
+		UserID:   in.UserID,
+		FullName: in.FullName,
+		Avatar:   in.Avatar,
+		Bio:      in.Bio,
+	})
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
 
-	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
+	stuctRes, err := structpb.NewValue(map[string]any{
+		"id":        res.GetID(),
+		"Full_name": res.GetFullName(),
+		"bio":       res.GetBio(),
+		"avatr":     res.GetAvatar(),
+	})
+
+	if err != nil {
+		return &profile_grpc.ProfileResponse{
+			Code:    400,
+			Message: err.Error(),
+			Result:  nil,
+		}, err
+	}
+
+	return &profile_grpc.ProfileResponse{
+		Code:    200,
+		Message: "success",
+		Result:  []*structpb.Value{stuctRes},
+	}, nil
+
 }
 func (_this *ProfileService) Delete(context.Context, *profile_grpc.DeleteProfileRequest) (*profile_grpc.ProfileResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Delete not implemented")
