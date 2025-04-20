42212505d468cf03cfbd63de710c10f92fc16efb
maruki00 <aitbaha48@gmail.com>
2025-04-20 22:00:00 +0100

    commit

--- internal/iam/infra/repositories/userRepository.go
+++ internal/iam/infra/repositories/userRepository.go
@@ -10,18 +10,6 @@ import (
 	"gorm.io/gorm"
 )
 
-/*
-	Save(ctx context.Context, user *models.User) error
-	Update(ctx context.Context, user *models.User) error
-	Delete(ctx context.Context, id string) error
-
-	FindByID(ctx context.Context, id string) (*models.User, error)
-	FindByEmail(ctx context.Context, email string) (*models.User, error)
-	FindByUsername(ctx context.Context, username string) (*models.User, error)
-	ListByTenant(ctx context.Context, tenantID string, pagination Pagination) ([]*models.User, error)
-
-*/
-
 type UserRepository struct {
 	db pkgPostgres.PGHandler
 }
