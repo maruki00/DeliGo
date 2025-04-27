13ecb623ca72abb05b5c03663e6f1227d2063e1b
maruki00 <aitbaha48@gmail.com>
2025-04-27 01:56:25 +0100

    commit

--- internal/shared/infra/models/pagination.go
+++ internal/shared/infra/models/pagination.go
@@ -1,17 +1,20 @@
 package shared_models
 
 type Pagination struct {
-	Page   int
-	Offset int
-	Limit  int
+	Page  int
+	Limit int
 }
 
-func (p *Pagination) GetPage() int {
-	return p.Page
+func (_this *Pagination) GetPage() int {
+	return _this.Page
 }
-func (p *Pagination) GetOffset() int {
-	return p.Offset
+func (_this *Pagination) GetOffset() int {
+
+	if _this.Page <= 0 {
+		_this.Page = 1
+	}
+	return (_this.Page - 1) * _this.Limit
 }
-func (p *Pagination) GetLimit() int {
-	return p.Limit
+func (_this *Pagination) GetLimit() int {
+	return _this.Limit
 }
