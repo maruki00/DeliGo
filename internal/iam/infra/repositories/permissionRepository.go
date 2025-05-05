9c6934e49b094c64a17a407dca5a5e53059b0a83
maruki00 <aitbaha48@gmail.com>
2025-05-05 23:06:35 +0100

    commit

--- internal/iam/infra/repositories/permissionRepository.go
+++ internal/iam/infra/repositories/permissionRepository.go
@@ -11,15 +11,17 @@ func NewPermissionRepository() *PermissionRepository {
 	return &PermissionRepository{}
 }
 
+func (_this *PermissionRepository) Save(ctx context.Context, permission *models.Permission) error {
+	return nil
+}
+
 func (_this *PermissionRepository) FindByID(ctx context.Context, id string) (*models.Permission, error) {
 	return nil, nil
 }
 func (_this *PermissionRepository) FindByPolicyID(ctx context.Context, policyID string) ([]*models.Permission, error) {
 	return nil, nil
 }
-func (_this *PermissionRepository) Save(ctx context.Context, permission *models.Permission) error {
-	return nil
-}
+
 func (_this *PermissionRepository) Delete(ctx context.Context, id string) error {
 	return nil
 }
