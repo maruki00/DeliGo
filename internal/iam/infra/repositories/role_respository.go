c1e341cb50eb31c9bc26d3d23b767130a7b92824
maruki00 <aitbaha48@gmail.com>
2025-05-06 00:48:51 +0100

    commit

--- internal/iam/infra/repositories/role_respository.go
+++ internal/iam/infra/repositories/role_respository.go
@@ -3,6 +3,9 @@ package repositories
 import (
 	"context"
 	"deligo/internal/iam/domain/entities"
+	valueobjects "deligo/internal/iam/domain/valueobject"
+	"deligo/internal/iam/infra/models"
+	shared_models "deligo/internal/shared/infra/models"
 	pkgPostgres "deligo/pkg/postgres"
 
 	"gorm.io/gorm"
@@ -30,17 +33,25 @@ func (_this *RoleRepository) Save(ctx context.Context, role entities.RoleEntity)
 	return err
 }
 
-func (_this *RoleRepository) GetByID(ctx context.Context, id string) (entities.RoleEntity, error) {
-
-	return nil, nil
+func (_this *RoleRepository) FindByID(ctx context.Context, id valueobjects.ID) (entities.RoleEntity, error) {
+	var role models.Role
+	if err := _this.db.DB.Where("id = ? ", string(id)).First(&role).Error; err != nil {
+		return nil, err
+	}
+	return &role, nil
 }
 
-func (_this *RoleRepository) GetByName(ctx context.Context, name string) (entities.RoleEntity, error) {
-
-	return nil, nil
+func (_this *RoleRepository) FindByName(ctx context.Context, name string) (entities.RoleEntity, error) {
+	var role models.Role
+	if err := _this.db.DB.Where("name = ? ", name).First(&role).Error; err != nil {
+		return nil, err
+	}
+	return &role, nil
 }
 
-func (_this *RoleRepository) List(ctx context.Context) ([]entities.RoleEntity, error) {
+func (_this *RoleRepository) List(ctx context.Context, pagination shared_models.Pagination) ([]entities.RoleEntity, error) {
+
+	var roles []*models.Role
 
 	return nil, nil
 }
