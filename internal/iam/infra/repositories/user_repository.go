d682be1f207bce35b34ce1d20bd283bb26cc216f
maruki00 <aitbaha48@gmail.com>
2025-05-09 00:53:17 +0100

    commit

--- internal/iam/infra/repositories/user_repository.go
+++ internal/iam/infra/repositories/user_repository.go
@@ -22,9 +22,6 @@ func NewUserRepository(db *pkgPostgres.PGHandler) *UserRepository {
 }
 
 func (ur *UserRepository) Save(ctx context.Context, entity entities.UserEntity) error {
-	//sql := `INSERT INTO "users" ("id","username","email","tenant_id","password","password_changed_at","is_active","last_login","mfa_enabled","mfa_secret","deleted_at")
-	// 				VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`
-
 	err := ur.db.DB.Transaction(func(tx *gorm.DB) error {
 		sql := `INSERT INTO "users" 
 			("id", "username", "email", "tenant_id", "password", "password_changed_at", "is_active", "last_login", "mfa_enabled", "mfa_secret", "deleted_at")
@@ -57,7 +54,6 @@ func (ur *UserRepository) Save(ctx context.Context, entity entities.UserEntity) 
 }
 
 func (ur *UserRepository) Delete(ctx context.Context, id valueobjects.ID) error {
-	//sql := `UPDATE users SET deleted_at = now(), updated_at = now() WHERE id = $1 `
 	err := ur.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Where("id = ?", id).Delete(&models.User{}).Error; err != nil {
 			tx.Rollback()
@@ -74,12 +70,6 @@ func (ur *UserRepository) Delete(ctx context.Context, id valueobjects.ID) error 
 }
 
 func (ur *UserRepository) Update(ctx context.Context, id valueobjects.ID, fields map[string]interface{}) error {
-	// sql := `
-	// 		UPDATE users
-	// 		SET email=$1, role=$2, updated_at = now()
-	// 		WHERE id=$3
-	// 		AND deleted_at = NULL
-	// 	`
 	err := ur.db.DB.Transaction(func(tx *gorm.DB) error {
 		err := tx.Model(&models.User{}).Where("id = ?", string(id)).Updates(fields).Error
 		if err != nil {
@@ -94,12 +84,6 @@ func (ur *UserRepository) Update(ctx context.Context, id valueobjects.ID, fields
 }
 
 func (ur *UserRepository) FindByID(ctx context.Context, id valueobjects.ID) (*models.User, error) {
-	// sql := `
-	// 		SELECT id,email,role
-	// 		FROM users
-	// 		WHERE id = $1
-	// 		LIMIT 1
-	// 	`
 	var user models.User
 	err := ur.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Where("id = ?", id).First(&user).Error; err != nil {
@@ -115,12 +99,6 @@ func (ur *UserRepository) FindByID(ctx context.Context, id valueobjects.ID) (*mo
 }
 
 func (ur *UserRepository) FindByEmail(ctx context.Context, email string) (*models.User, error) {
-	// sql := `
-	// 		SELECT id,email,role
-	// 		FROM users
-	// 		WHERE id = $1
-	// 		LIMIT 1
-	// 	`
 	var user models.User
 	err := ur.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Where("email = ?", email).First(&user).Error; err != nil {
