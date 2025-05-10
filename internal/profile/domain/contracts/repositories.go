ca364d75815839125cd2f85a576439150ae085a1
maruki00 <aitbaha48@gmail.com>
2025-05-10 11:44:38 +0100

    commit

--- internal/profile/domain/contracts/repositories.go
+++ internal/profile/domain/contracts/repositories.go
@@ -8,9 +8,10 @@ import (
 type Pagination struct{}
 
 type IPorofileRepository interface {
-	FindByID(ctx context.Context, id string) (*models.Policy, error)
+	Save(ctx context.Context, policy *models.Profile) error
+	Disable(ctx context.Context, policy *models.Profile) error
+	FindByUserID(ctx context.Context, id string) (*models.Policy, error)
 	FindByName(ctx context.Context, name string) (*models.Policy, error)
-	Save(ctx context.Context, policy *models.Policy) error
 	Delete(ctx context.Context, id string) error
 	ListForTenant(ctx context.Context, tenantID string) ([]*models.Policy, error)
 }
