8255591ee3210965f0b9e4deb50b0ba316d410f4
maruki00 <aitbaha48@gmail.com>
2025-05-18 01:43:39 +0100

    commit

--- internal/profile/infra/repositories/profileRepository.go
+++ internal/profile/infra/repositories/profileRepository.go
@@ -19,10 +19,16 @@ func NewProfileRepository(db pkgPostgres.DBHandler) *ProfileRepository {
 	}
 }
 
-func (_this *ProfileRepository) Save(ctx context.Context, entity *entities.ProfileEntity) error {
+func (_this *ProfileRepository) Save(ctx context.Context, entity entities.ProfileEntity) error {
 	err := _this.db.GetDB().Transaction(func(tx *gorm.DB) error {
-		sql := `INSERT INTO profiles (id, user_name, full_name, avatar, bio) VALUES (?, ?, ?, ?, ?)`
-		if err := tx.Exec(sql, entity.GetID(), entity.GetUserI(), entity.GetFullNam(), entity.GetAvata(), entity.GetBio()).Error; err != nil {
+		sql := `INSERT INTO profiles (id, user_id, full_name, avatar, bio) VALUES (?, ?, ?, ?, ?)`
+		if err := tx.Exec(sql,
+			entity.GetID(),
+			entity.GetUserID(),
+			entity.GetFullName(),
+			entity.GetAvatar(),
+			entity.GetBio(),
+		).Error; err != nil {
 			return err
 		}
 		return nil
@@ -33,9 +39,9 @@ func (_this *ProfileRepository) Save(ctx context.Context, entity *entities.Profi
 	return nil
 }
 
-func (_this *ProfileRepository) Disable(ctx context.Context, profile *models.Profile) error {
-	return _this.db.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Model(&tx.Model.Profile).Where("id = ?", id).UpdateColumn("is_active", false).Error; err != nil {
+func (_this *ProfileRepository) Disable(ctx context.Context, id string) error {
+	return _this.db.GetDB().Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&models.Profile{}).Where("id = ?", id).UpdateColumn("is_active", false).Error; err != nil {
 			tx.Rollback()
 			return err
 		}
@@ -53,8 +59,15 @@ func (_this *ProfileRepository) FindByUserID(ctx context.Context, id string) (*m
 }
 
 func (_this *ProfileRepository) Update(context.Context, string, map[string]any) error {
+
 	return nil
 }
-func (_this *ProfileRepository) UpdateAvatar(context.Context, string, string) error {
-	return nil
+func (_this *ProfileRepository) UpdateAvatar(ctx context.Context, id string, avatar string) error {
+	return _this.db.GetDB().Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&models.Profile{}).Where("id = ?", id).UpdateColumn("avatar", avatar).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+		return nil
+	})
 }
