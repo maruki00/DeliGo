88e7a5d281e053c57c95a0c0d7a7354778d4917e
maruki00 <aitbaha48@gmail.com>
2025-05-27 23:15:48 +0100

    commit

--- internal/product/domian/repositories/product_repository.go
+++ internal/product/domian/repositories/product_repository.go
@@ -6,10 +6,10 @@ import (
 )
 
 type IProductRepository interface {
-	Save(ctx context.Context, product entities.ProductEntity) (entities.ProductEntity, error)
-	Find(ctx context.Context, id string) (entities.ProductEntity, error)
-	Update(ctx context.Context, id string, data map[string]interface{}) (entities.ProductEntity, error)
-	Delete(ctx context.Context, id string) (entities.ProductEntity, error)
-	List(ctx context.Context) ([]entities.ProductEntity, error)
-	// GetProductByMultipleId(ctx context.Context, ids []int) ([]models.Product, error)
+	Save(context.Context, entities.ProductEntity) error
+	Find(context.Context, string) (entities.ProductEntity, error)
+	Update(context.Context, string, map[string]any) error
+	Delete(context.Context, string) error
+	List(context.Context) ([]entities.ProductEntity, error)
+	GetProductByMultipleId(context.Context, []string) ([]entities.ProductEntity, error)
 }
