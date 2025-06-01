b65684180b9c8e9a786f06bc51b529b130171662
maruki00 <aitbaha48@gmail.com>
2025-06-01 05:35:31 +0100

    commit

--- internal/shop/app/services/shop_server_service.go
+++ internal/shop/app/services/shop_server_service.go
@@ -1,6 +1,11 @@
 package services
 
-import pkgCqrs "deligo/pkg/cqrs"
+import (
+	"context"
+	pkgCqrs "deligo/pkg/cqrs"
+
+	"google.golang.org/grpc"
+)
 
 type ShopServerService struct {
 	cmdBus *pkgCqrs.CommandBus
@@ -13,4 +18,25 @@ func NewShopServerService(cmdBus *pkgCqrs.CommandBus, qryBus *pkgCqrs.QueryBus) 
 		qryBus: qryBus,
 	}
 	return &srv
+}
+
+func (_this *ShopServerService) Save(ctx context.Context, in *shop_grpc.CreateShopRequest, opts ...grpc.CallOption) (*ShopResponse, error) {
+	return nil, nil
+}
+
+func (_this *ShopServerService) Update(ctx context.Context, in *UpdateShopRequest, opts ...grpc.CallOption) (*ShopResponse, error) {
+	return nil, nil
+}
+
+func (_this *ShopServerService) Delete(ctx context.Context, in *UpdateShopStatusRequest, opts ...grpc.CallOption) (*ShopResponse, error) {
+	return nil, nil
+}
+
+func (_this *ShopServerService) GetShopStatus(ctx context.Context, in *GETRequest, opts ...grpc.CallOption) (*ShopResponse, error) 	
+return nil, nil
+}
+
+func (_this *ShopServerService) GetShop(ctx context.Context, in *GETRequest, opts ...grpc.CallOption) (*ShopResponse, error) {
+
+	return nil, nil
 }
