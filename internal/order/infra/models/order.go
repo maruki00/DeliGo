5e5da00b943be46daf8177bc05743cae6404d866
maruki00 <aitbaha48@gmail.com>
2025-06-02 03:22:23 +0100

    commit

--- internal/order/infra/models/order.go
+++ internal/order/infra/models/order.go
@@ -18,15 +18,3 @@ type Order struct {
 	OrderDate       time.Time             `json:"order_date"`
 	PayementID      shared_valueobject.ID `json:"payement_id"`
 }
-
-// id VARCHAR(36) PRIMARY KEY NOT NULL,
-//     customer_id VARCHAR(36) NOT NULL,
-//     order_status VARCHAR(50) NOT NULL,
-//     total_amount DECIMAL(10, 2) NOT NULL,
-//     currency VARCHAR(3) NOT NULL,
-//     order_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-//     shipping_address JSONB NULL,
-//     billing_address JSONB NULL,
-//     payment_id VARCHAR(36) NULL,
-//     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-//     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
