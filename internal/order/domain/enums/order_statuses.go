2988a370201394c6dba1c8cfd0b22f51b478ec6c
maruki00 <aitbaha48@gmail.com>
2025-06-02 01:09:50 +0100

    commit

---
+++ internal/order/domain/enums/order_statuses.go
@@ -0,0 +1,39 @@
+package enums
+
+type OrderStatus int
+
+const (
+	OrderStatusUnknown OrderStatus = iota
+	OrderStatusCreated
+	OrderStatusPaid
+	OrderStatusPending
+	OrderStatusProcessing
+	OrderStatusShipped
+	OrderStatusDelivered
+	OrderStatusCancelled
+	OrderStatusRefunded
+)
+
+func (_this OrderStatus) String() string {
+	switch _this {
+	case OrderStatusUnknown:
+		return "Unknown"
+	case OrderStatusCreated:
+		return "Created"
+	case OrderStatusPaid:
+		return "Paid"
+	case OrderStatusPending:
+		return "Pending"
+	case OrderStatusProcessing:
+		return "Processing"
+	case OrderStatusShipped:
+		return "Shipped"
+	case OrderStatusDelivered:
+		return "Delivered"
+	case OrderStatusCancelled:
+		return "Cancelled"
+	case OrderStatusRefunded:
+		return "Refunded"
+	}
+	return "Unknown"
+}
--- internal/order/infra/models/order.go
+++ internal/order/infra/models/order.go
@@ -1,34 +1,13 @@
 package models
 
 import (
-	"time"
-
-	"gorm.io/gorm"
+	shared_models "deligo/internal/shared/infra/models"
 )
 
 type Order struct {
-	gorm.Model
-	ID               int       `json:"id"`
-	OrderFingerprint string    `json:"order_fingerprint"`
-	CostumerId       int       `json:"costumer_id"`
-	Cost             float32   `json: "cost"`
-	Status           int       `json: "status"`
-	CreatedAt        time.Time `json:"created_at"`
-	UpdatedAt        time.Time `json:"updated_at"`
-}
-
-func (obj *Order) GetId() int {
-	return obj.Id
-}
-func (obj *Order) GetOrderFingerprint() string {
-	return obj.OrderFingerprint
-}
-func (obj *Order) GetCostumerId() int {
-	return obj.CostumerId
-}
-func (obj *Order) GetCost() float32 {
-	return obj.Cost
-}
-func (obj *Order) GetStatus() int {
-	return obj.Status
+	shared_models.BaseModel
+	OrderFingerprint string  `json:"order_fingerprint"`
+	CostumerId       int     `json:"costumer_id"`
+	Cost             float32 `json:"cost"`
+	Status           int     `json:"status"`
 }
