be5cf5627eaa11f30fb1c4b3bb5c6b8405ce1a51
maruki00 <aitbaha48@gmail.com>
2025-06-09 20:59:26 +0100

    commit

--- internal/iam/infra/grpc/role/role.pb.gw.go
+++ internal/iam/infra/grpc/role/role.pb.gw.go
@@ -318,7 +318,7 @@ func RegisterRoleServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 // RegisterRoleServiceHandlerFromEndpoint is same as RegisterRoleServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterRoleServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.NewClient(endpoint, opts...)
+	conn, err := grpc.Dial(endpoint, opts...)
 	if err != nil {
 		return err
 	}
