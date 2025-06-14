c105a8db7169e71aee94ea0d40575d4e81a7835d
maruki00 <aitbaha48@gmail.com>
2025-06-14 19:56:05 +0100

    commit

--- internal/shop/app/service/shop_server_service.go
+++ internal/shop/app/service/shop_server_service.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"errors"
+	"time"
 
 	sharedvo "github.com/maruki00/deligo/internal/shared/value_object"
 	"github.com/maruki00/deligo/internal/shop/app/command"
@@ -15,8 +17,6 @@ type ShopServerService struct {
 	cmdBus *pkgCqrs.CommandBus
 	qryBus *pkgCqrs.QueryBus
 }
-
-var response = &shop_grpc.ShopResponse{}
 
 func NewShopServerService(cmdBus *pkgCqrs.CommandBus, qryBus *pkgCqrs.QueryBus) *ShopServerService {
 	srv := ShopServerService{
@@ -32,17 +32,45 @@ func (_this *ShopServerService) Save(ctx context.Context, in *shop_grpc.CreateSh
 
 func (_this *ShopServerService) Update(ctx context.Context, in *shop_grpc.UpdateShopRequest, opts ...grpc.CallOption) (*shop_grpc.ShopResponse, error) {
 
-	cmd := command.UpdateShopCommand{
-		ID:       sharedvo.NewID(in.ID),
-		ShopName: in.ShopName,
-		OpenAt:   in.OpenAt,
-		CloseAt:  in.CloseAt,
+	id := sharedvo.Parse(in.ID)
+	if id == "" {
+		return nil, errors.New("invalid id")
 	}
 
-	return response, nil
+	openAt, err := time.Parse("2025-05-14", in.OpenAt)
+	if err != nil {
+		return nil, errors.New("invalid time on open_at")
+	}
+
+	closeAt, err := time.Parse("2025-05-14", in.CloseAt)
+	if err != nil {
+		return nil, errors.New("invalid time on close_at")
+	}
+
+	cmd := command.UpdateShopCommand{
+		ID:       id,
+		ShopName: in.ShopName,
+		OpenAt:   openAt,
+		CloseAt:  closeAt,
+	}
+
+	err = _this.cmdBus.Dispatch(ctx, &cmd)
+	if err != nil {
+		return nil, err
+	}
+	return &shop_grpc.ShopResponse{
+		Code:    200,
+		Message: "success",
+	}, nil
 }
 
 func (_this *ShopServerService) Delete(ctx context.Context, in *shop_grpc.UpdateShopStatusRequest, opts ...grpc.CallOption) (*shop_grpc.ShopResponse, error) {
+	id := sharedvo.Parse(in.ID)
+	if id == "" {
+		return nil, errors.New("invalid id")
+	}
+	if err := _this.cmdBus.Dispatch(ctx, cmd) 
+
 	return nil, nil
 }
 
