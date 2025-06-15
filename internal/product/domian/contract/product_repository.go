dbf25f9ede55bd52dd75ccc9cf1c55249c003d15
maruki00 <aitbaha48@gmail.com>
2025-06-15 19:47:46 +0100

    commit

--- internal/product/domian/contract/product_repository.go
+++ internal/product/domian/contract/product_repository.go
@@ -10,9 +10,9 @@ import (
 type IProductRepository interface {
 	Save(context.Context, *model.Product) error
 	GetById(context.Context, sharedvo.ID) (*model.Product, error)
-	List(context.Context, string) ([]*model.Product, error)
+	Search(context.Context, string) ([]*model.Product, error)
 	Update(context.Context, sharedvo.ID, *model.Product) error
 	Delete(context.Context, sharedvo.ID) error
-	GetManyProductsByID(context.Context, []sharedvo.ID) ([]*model.Product, error)
-	GetProductByMultipleId(context.Context, []sharedvo.ID) ([]*model.Product, error)
+	GetMany(context.Context, []sharedvo.ID) ([]*model.Product, error)
+	GetByIds(context.Context, []sharedvo.ID) ([]*model.Product, error)
 }
--- internal/product/infra/repository/productRepsoitory.go
+++ internal/product/infra/repository/productRepsoitory.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/maruki00/deligo/internal/product/infra/model"
 	sharedvo "github.com/maruki00/deligo/internal/shared/value_object"
+	"github.com/maruki00/deligo/pkg/pagination"
 	pkgPostgres "github.com/maruki00/deligo/pkg/postgres"
 
 	"gorm.io/gorm"
@@ -20,6 +21,7 @@ func NewProductRepository(db pkgPostgres.PGHandler) *ProductRepository {
 	}
 }
 
+// Commands
 func (_this *ProductRepository) Save(ctx context.Context, product *model.Product) error {
 	return _this.db.GetDB().Transaction(func(tx *gorm.DB) error {
 		if err := _this.db.GetDB().Model(&model.Product{}).Create(product).Error; err != nil {
@@ -28,23 +30,6 @@ func (_this *ProductRepository) Save(ctx context.Context, product *model.Product
 		return nil
 	})
 }
-
-func (_this *ProductRepository) GetById(ctx context.Context, id sharedvo.ID) (*model.Product, error) {
-	var product model.Product
-	if err := _this.db.GetDB().Where("where = ? ", id).Find(&product).Error; err != nil {
-		return nil, err
-	}
-	return &product, nil
-}
-
-func (_this *ProductRepository) List(ctx context.Context, search string) ([]*model.Product, error) {
-	var items []*model.Product
-	if err := _this.db.GetDB().Model(&model.Product{}).Find(&items).Error; err != nil {
-		return nil, err
-	}
-	return items, nil
-}
-
 func (_this *ProductRepository) Update(ctx context.Context, id sharedvo.ID, product *model.Product) error {
 	return _this.db.GetDB().Transaction(func(tx *gorm.DB) error {
 		if err := tx.Model(&model.Product{}).Where("id = ?", id).Updates(product).Error; err != nil {
@@ -64,6 +49,27 @@ func (_this *ProductRepository) Delete(ctx context.Context, id sharedvo.ID) erro
 		}
 		return nil
 	})
+}
+
+// 	Search(context.Context, string) ([]*model.Product, error)
+// 	GetMany(context.Context, []sharedvo.ID) ([]*model.Product, error)
+// 	GetByIds(context.Context, []sharedvo.ID) ([]*model.Product, error)
+
+// Queries
+func (_this *ProductRepository) GetById(ctx context.Context, id sharedvo.ID) (*model.Product, error) {
+	var product model.Product
+	if err := _this.db.GetDB().Where(" id = ? ", id).Find(&product).Error; err != nil {
+		return nil, err
+	}
+	return &product, nil
+}
+
+func (_this *ProductRepository) Search(ctx context.Context, search string, pagination pagination.Pagination) ([]*model.Product, error) {
+	var items []*model.Product
+	if err := _this.db.GetDB().Model(&model.Product{}).Limit().Offset().Find(&items).Error; err != nil {
+		return nil, err
+	}
+	return items, nil
 }
 
 func (_this *ProductRepository) GetManyProductsByID(ctx context.Context, ids []sharedvo.ID) ([]*model.Product, error) {
