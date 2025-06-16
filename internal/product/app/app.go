74664644b1bf6f72a05a39a8f46241d2ac179832
maruki00 <aitbaha48@gmail.com>
2025-06-16 01:43:58 +0100

    commit

--- internal/product/app/app.go
+++ internal/product/app/app.go
@@ -3,14 +3,37 @@ package app
 import (
 	"context"
 
+	pkgCqrs "github.com/maruki00/deligo/pkg/cqrs"
+	pkgPostgres "github.com/maruki00/deligo/pkg/postgres"
 	"github.com/rabbitmq/amqp091-go"
 )
 
 type App struct {
+
+	DB *pkgPostgres.DBHandler
+
+	CmdBus *pkgCqrs.CommandBus
+	QryBus *pkgCqrs.QueryBus
+
+
 }
 
-func Init() *App {
-	return &App{}
+func Init(cgfPath string) *App {
+	
+	db, err := pkgPostgres.NewDB("")
+	if err != nil {
+		panic(err)
+	}
+
+	cmdBus := pkgCqrs.NewCommandBus()
+	qryBus := pkgCqrs.NewQueryBus()
+
+
+	return &App{
+		DB: db,
+		CmdBus: cmdBus,
+		QryBus: qryBus,
+	}
 }
 func (app *App) Worder(ctx context.Context, github.com/maruki00/deligo <-chan amqp091.Delivery) {
 
