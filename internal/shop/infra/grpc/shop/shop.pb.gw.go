78663ec70d04ef8c34e5e57fddab63fd98f73b17
maruki00 <aitbaha48@gmail.com>
2025-06-25 23:39:33 +0100

    commit

--- internal/shop/infra/grpc/shop/shop.pb.gw.go
+++ internal/shop/infra/grpc/shop/shop.pb.gw.go
@@ -318,7 +318,7 @@ func RegisterProfileServiceHandlerServer(ctx context.Context, mux *runtime.Serve
 // RegisterProfileServiceHandlerFromEndpoint is same as RegisterProfileServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterProfileServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
