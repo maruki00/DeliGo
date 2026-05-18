bda5134eb891218033df274839e5230348798e60
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-05-18 18:00:00 +0300

    Update authz.go

--- internal/iam/app/service/authz.go
+++ internal/iam/app/service/authz.go
@@ -2,8 +2,6 @@ package services
 
 import (
 	"context"
-
-	"github.com/maruki00/deligo/internal/iam/infra/repository"
 )
 
 type AuthzService struct {
@@ -14,4 +12,6 @@ func NewAuthzService(repo *repository.AuthzRepository) *AuthzService {
 	return &AuthzService{repo: repo}
 }
 
-func (_this *AuthzService) SavePolicy(ctx context.Context)
+func (_this *AuthzService) SavePolicy(ctx context.Context) {
+
+}
