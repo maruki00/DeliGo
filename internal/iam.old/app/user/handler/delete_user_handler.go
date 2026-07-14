95b60f727cb6e38a247338013b3827f373bd7b28
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 09:20:59 +0300

    Add delete_user_handler.go

---
+++ internal/iam.old/app/user/handler/delete_user_handler.go
@@ -0,0 +1,28 @@
+package userHandler
+
+import (
+	"context"
+	"fmt"
+
+	userCommand "github.com/maruki00/deligo/internal/iam/app/user/command"
+	"github.com/maruki00/deligo/internal/iam/domain/contract"
+	sharedvo "github.com/maruki00/deligo/internal/shared/value_object"
+	pkgCqrs "github.com/maruki00/deligo/pkg/cqrs"
+)
+
+type DeleteUserHandler struct {
+	userRepo contract.IUserRepository
+}
+
+func NewDeleteUserHandler(userRepo contract.IUserRepository) *DeleteUserHandler {
+	return &DeleteUserHandler{
+		userRepo: userRepo,
+	}
+}
+
+func (_this *DeleteUserHandler) Handle(ctx context.Context, command pkgCqrs.Command) error {
+	cmd := command.(*userCommand.DeleteUserCommand)
+	fmt.Println("hello worldf")
+	err := _this.userRepo.Delete(ctx, sharedvo.ID(cmd.ID.String()))
+	return err
+}
