fc11d990c9458652c2399d429fb565f5f0914d0d
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 09:21:01 +0300

    Update id.go

--- internal/iam/domain/valueobject/id.go
+++ internal/iam/domain/valueobject/id.go
@@ -9,6 +9,14 @@ func NewID() ID {
 	return ID(id.String())
 }
 
+func GetID(id string) ID {
+	if _, err := uuid.Parse(id); err != nil {
+		// check if id is empty then not valid id
+		return ID("")
+	}
+	return ID(id)
+}
+
 func (_this *ID) String() string {
 	return string(*_this)
 }
