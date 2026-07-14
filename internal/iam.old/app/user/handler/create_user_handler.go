230e89760530f39e8f7c6422eeb8ba00cc83a2b7
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 09:20:59 +0300

    Add create_user_handler.go

---
+++ internal/iam.old/app/user/handler/create_user_handler.go
@@ -0,0 +1,41 @@
+package userHandler
+
+import (
+	"context"
+
+	userCommand "github.com/maruki00/deligo/internal/iam/app/user/command"
+	"github.com/maruki00/deligo/internal/iam/domain/contract"
+	valueobjects "github.com/maruki00/deligo/internal/iam/domain/valueobject"
+	"github.com/maruki00/deligo/internal/iam/infra/model"
+	pkgCqrs "github.com/maruki00/deligo/pkg/cqrs"
+)
+
+type CreateUserHandler struct {
+	userRepo contract.IUserRepository
+}
+
+func NewCreateUserHandler(userRepo contract.IUserRepository) *CreateUserHandler {
+	return &CreateUserHandler{
+		userRepo: userRepo,
+	}
+}
+
+func (_this *CreateUserHandler) Handle(ctx context.Context, command pkgCqrs.Command) error {
+	cmd := command.(*userCommand.CreateUserCommand)
+	pwd, err := valueobjects.NewPassword(cmd.Password)
+	if err != nil {
+		return err
+	}
+
+	err = _this.userRepo.Save(ctx, &model.User{
+		ID:                valueobjects.ID(cmd.ID.String()),
+		Username:          cmd.Username,
+		Email:             cmd.Email,
+		Password:          pwd,
+		PasswordChangedAt: nil,
+		IsActive:          false,
+		MFAEnabled:        false,
+		MFASecret:         "",
+	})
+	return err
+}
