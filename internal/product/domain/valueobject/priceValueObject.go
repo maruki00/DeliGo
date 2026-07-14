20bf8fdb67954835f356919d8aa46b4dd43239ff
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 09:20:59 +0300

    Add priceValueObject.go

---
+++ internal/product/domain/valueobject/priceValueObject.go
@@ -0,0 +1,22 @@
+package valueobjects
+
+import (
+	"fmt"
+)
+
+type PriceVauleObject struct {
+	price float32
+}
+
+func NewPriceValueObject(price float32) *PriceVauleObject {
+	if price == 0.0 {
+		panic("invalid price")
+	}
+	return &PriceVauleObject{
+		price: price,
+	}
+}
+
+func (p *PriceVauleObject) String() string {
+	return fmt.Sprintf("%f", p.price)
+}
