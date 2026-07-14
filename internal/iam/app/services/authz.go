fffca1aa968e880da16de258b45e32953b827f60
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 10:58:33 +0300

    Update authz.go

--- internal/iam/app/services/authz.go
+++ internal/iam/app/services/authz.go
@@ -1,11 +1,9 @@
 package services
 
-import "github.com/maruki00/deligo/internal/iam/infra/repository"
+// type AuthzService struct {
+// 	repo repository.PolicyRepository
+// }
 
-type AuthzService struct {
-	repo repository.PolicyRepository
-}
-
-func NewAuthzService(repo repository.PolicyRepository) *AuthzService {
-	return &AuthzService{repo: repo}
-}
+// func NewAuthzService(repo con.PolicyRepository) *AuthzService {
+// 	return &AuthzService{repo: repo}
+// }
