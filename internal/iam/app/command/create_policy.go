4dcb4e1c73d72bde5b899df349a542d0903ac1b8
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-14 22:24:03 +0300

    Update create_policy.go

--- internal/iam/app/command/create_policy.go
+++ internal/iam/app/command/create_policy.go
@@ -1,4 +1,29 @@
 package command
 
+import (
+	"github.com/maruki00/deligo/internal/iam/infra/model"
+)
+
 type CreatePolicy struct {
+	Role       string  // V0
+	Resource   string  // V1
+	Action     string  // V2
+	Effect     *string // V3
+	IpSource   *string // V4
+	TimeWindow *string // V5
+}
+
+func (_this *CreatePolicy) Name() string {
+	return "CreatePolicy"
+}
+
+func (_this *CreatePolicy) MapToModel() *model.Policy {
+	return &model.Policy{
+		Role:       _this.Role,
+		Resource:   _this.Resource,
+		Action:     _this.Action,
+		Effect:     _this.Effect,
+		IpSource:   _this.IpSource,
+		TimeWindow: _this.TimeWindow,
+	}
 }
