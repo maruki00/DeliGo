c70d6e556aa58afcc1eac10000daa395b72f6d3f
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-17 20:07:27 +0300

    Update user_repo.go

--- internal/iam/infra/data/postgres/user_repo.go
+++ internal/iam/infra/data/postgres/user_repo.go
@@ -2,8 +2,10 @@ package postgres
 
 import (
 	"context"
+	"errors"
 
-	"github.com/maruki00/deligo/internal/iam/infra/model"
+	"github.com/maruki00/deligo/internal/iam/domain/entity"
+	"github.com/maruki00/deligo/internal/iam/infra/mapper"
 	pkgPostgres "github.com/maruki00/deligo/pkg/postgres"
 	"gorm.io/gorm"
 )
@@ -18,7 +20,7 @@ func NewUserRepo(db *pkgPostgres.PGHandler) *UserRepo {
 	}
 }
 
-func (_this *UserRepo) Save(ctx context.Context, entity *model.User) error {
+func (_this *UserRepo) Save(ctx context.Context, entity *entity.User) error {
 	tx := _this.db.DB.Save(entity)
 	if tx.Error != nil {
 		return tx.Error
@@ -26,8 +28,8 @@ func (_this *UserRepo) Save(ctx context.Context, entity *model.User) error {
 	return nil
 }
 
-func (_this *UserRepo) FindByID(ctx context.Context, id string) (*model.User, error) {
-	user := model.User{}
+func (_this *UserRepo) FindByID(ctx context.Context, id string) (*entity.User, error) {
+	user := entity.User{}
 	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Model(&user).Where("id = ?", id).Find(&user).Error; err != nil {
 			return err
@@ -41,8 +43,8 @@ func (_this *UserRepo) FindByID(ctx context.Context, id string) (*model.User, er
 	return &user, nil
 }
 
-func (_this *UserRepo) FindByUserReponame(ctx context.Context, username string) (*model.User, error) {
-	user := model.User{}
+func (_this *UserRepo) FindByUsername(ctx context.Context, username string) (*entity.User, error) {
+	user := entity.User{}
 	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Model(&user).Where("username = ?", username).Find(&user).Error; err != nil {
 			return err
@@ -56,8 +58,8 @@ func (_this *UserRepo) FindByUserReponame(ctx context.Context, username string) 
 	return &user, nil
 }
 
-func (_this *UserRepo) FindByEmail(ctx context.Context, email string) (*model.User, error) {
-	user := model.User{}
+func (_this *UserRepo) FindByEmail(ctx context.Context, email string) (*entity.User, error) {
+	user := entity.User{}
 	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Model(&user).Where("email = ?", email).Find(&user).Error; err != nil {
 			return err
@@ -73,7 +75,7 @@ func (_this *UserRepo) FindByEmail(ctx context.Context, email string) (*model.Us
 
 func (_this *UserRepo) Delete(ctx context.Context, id string) error {
 	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Delete(&model.User{}, map[string]interface{}{
+		if err := tx.Delete(&entity.User{}, map[string]interface{}{
 			"id": id,
 		}).Error; err != nil {
 			return err
@@ -88,10 +90,31 @@ func (_this *UserRepo) Delete(ctx context.Context, id string) error {
 	return nil
 }
 
-func (_this *UserRepo) FindAll(ctx context.Context) ([]*model.User, error) {
-	var users []*model.User
+func (_this *UserRepo) StatusUpdate(ctx context.Context, id, status string) error {
+	if status != "ban" && status != "active" {
+		return errors.New("status not allowed")
+	}
+	if id != "1" {
+		return errors.New("user not found")
+	}
 	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Model(&model.User{}).Find(&users).Error; err != nil {
+		if err := tx.Where("id", id).Update("status", status).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (_this *UserRepo) FindAll(ctx context.Context, page, offset int) ([]*entity.User, error) {
+	var users []*entity.User
+	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Model(&entity.User{}).Find(&users).Error; err != nil {
 			return err
 		}
 		return nil
@@ -104,40 +127,9 @@ func (_this *UserRepo) FindAll(ctx context.Context) ([]*model.User, error) {
 	return users, nil
 }
 
-func (_this *UserRepo) FindByTenantID(ctx context.Context, tenantID string) ([]*model.User, error) {
-	var users []*model.User
-	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Model(&model.User{}).Where("tenant_id = ?", tenantID).Find(&users).Error; err != nil {
-			return err
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
-func (_this *UserRepo) FindByTenantIDAndRole(ctx context.Context, tenantID string, role string) ([]*model.User, error) {
-	var users []*model.User
-	err := _this.db.DB.Transaction(func(tx *gorm.DB) error {
-		if err := tx.Model(&model.User{}).Where("tenant_id = ? AND role = ?", tenantID, role).Find(&users).Error; err != nil {
-			return err
-		}
-		return nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return users, nil
-}
-
-func (_this *UserRepo) Update(ctx context.Context, entity *model.User) error {
-	tx := _this.db.DB.Save(entity)
+func (_this *UserRepo) Update(ctx context.Context, entity *entity.User) error {
+	user := mapper.ToGormModel(entity)
+	tx := _this.db.DB.Save(user)
 	if tx.Error != nil {
 		return tx.Error
 	}
