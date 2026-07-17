705acf6e16e8c047391e4686a1db292461c8fab0
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-17 20:07:26 +0300

    Add main.go

---
+++ cmd/shop/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// instance, cleanup, err := app.Init()
+	// if err != nil {
+	// 	log.Fatalf("failed to initialize shop service: %v", err)
+	// }
+	// defer cleanup()
+
+	// router := api.NewRouter(instance)
+	// log.Printf("shop service initialized: %s", instance.Health())
+	// if err := router.Run(":8090"); err != nil {
+	// 	log.Fatalf("failed to serve shop service: %v", err)
+	// }
+}
