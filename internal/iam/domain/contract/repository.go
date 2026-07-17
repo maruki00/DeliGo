a5023e9e70566e602fe6e3da54ab600e2bd073cf
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-17 20:07:27 +0300

    Update repository.go

--- internal/iam/domain/contract/repository.go
+++ internal/iam/domain/contract/repository.go
@@ -1,10 +1,22 @@
 package contract
 
-type AuthzRepository interface {
-}
+import (
+	"context"
 
-type AuthnRepository interface {
-}
+	"github.com/maruki00/deligo/internal/iam/infra/model"
+)
 
 type UserRepository interface {
+	Create(ctx context.Context, user *model.UserGormModel) error
+	GetByID(ctx context.Context, id string) (*model.UserGormModel, error)
+	GetByEmail(ctx context.Context, email string) (*model.UserGormModel, error)
+	Update(ctx context.Context, user *model.UserGormModel) error
+}
+
+type IdentityRepository interface {
+	AddPolicy(sub, obj, act string) (bool, error)
+	AddGroupPolicy(sub, group string) (bool, error)
+	RemovePolicy(sub, obj, act string) (bool, error)
+	RemoveGroupPolicy(sub, group string) (bool, error)
+	CheckPermission(sub, obj, act string) (bool, error)
 }
