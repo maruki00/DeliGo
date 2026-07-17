30164525d6d0e6d35fed1fc8787e658fb8b5f63f
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-17 20:07:27 +0300

    Update authz_repo.go

--- internal/iam/infra/data/postgres/authz_repo.go
+++ internal/iam/infra/data/postgres/authz_repo.go
@@ -9,33 +9,46 @@ import (
 	pkgPostgres "github.com/maruki00/deligo/pkg/postgres"
 )
 
-type AuthzRepo struct {
-	db    *pkgPostgres.PGHandler
+type IdentityRepo struct {
 	authz *pkgAuth.Authz
 }
 
-func NewAuthzRepo(db *pkgPostgres.PGHandler, authz *pkgAuth.Authz) *AuthzRepo {
-	return &AuthzRepo{
-		db:    db,
+func NewIdentityRepo(db *pkgPostgres.PGHandler, authz *pkgAuth.Authz) *IdentityRepo {
+	return &IdentityRepo{
 		authz: authz,
 	}
 }
 
-func (_this *AuthzRepo) SavePolicy(ctx context.Context, policy *model.Policy) error {
+func (_this *IdentityRepo) SavePolicy(ctx context.Context, policy *model.Policy) error {
 	mapped := policy.Map2Casbin()
 
-	err := _this.authz.AddPolicy(ctx, mapped.V0, mapped.V1, mapped.V2, *mapped.V3, *mapped.V4, *mapped.V5)
+	err := _this.authz.AddPolicy(ctx, mapped.V0, mapped.V1, mapped.V2)
 	if err != nil {
 		return fmt.Errorf("error saving new policy: %v\n", err.Error())
 	}
 	return nil
 }
 
-func (_this *AuthzRepo) SaveGoupPolicy(ctx context.Context, group *model.GroupPolicy) error {
+func (_this *IdentityRepo) SaveGoupPolicy(ctx context.Context, group *model.GroupPolicy) error {
 	mapped := group.Map2Casbin()
 	return _this.authz.AddGroupingPolicy(ctx, mapped.V0, mapped.V1, mapped.V2)
 }
 
-func (_this *AuthzRepo) Check(ctx context.Context, sub, obj, act string) (bool, error) {
+func (_this *IdentityRepo) RemovePolicy(ctx context.Context, policy *model.Policy) error {
+	mapped := policy.Map2Casbin()
+
+	err := _this.authz.RmPolicy(ctx, mapped.V0, mapped.V1, mapped.V2)
+	if err != nil {
+		return fmt.Errorf("error saving new policy: %v\n", err.Error())
+	}
+	return nil
+}
+
+func (_this *IdentityRepo) RemoveGoupPolicy(ctx context.Context, group *model.GroupPolicy) error {
+	mapped := group.Map2Casbin()
+	return _this.authz.RmGroupingPolicy(ctx, mapped.V0, mapped.V1, mapped.V2)
+}
+
+func (_this *IdentityRepo) Check(ctx context.Context, sub, obj, act string) (bool, error) {
 	return _this.authz.Check(ctx, sub, obj, act)
 }
