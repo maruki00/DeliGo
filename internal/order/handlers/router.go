30c66796b2efab228d49093fc62424437b4ee4e2
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-22 22:22:39 +0300

    Add router.go

---
+++ internal/order/handlers/router.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"github.com/maruki00/deligo/order/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(handler *OrderHandler) *gin.Engine {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.Use(middleware.GlobalErrorHandler())
+
+	orders := r.Group("/orders")
+	{
+		orders.POST("", handler.Create)
+		orders.GET("/:id", handler.GetByID)
+		orders.POST("/:id/confirm", handler.Confirm)
+		orders.POST("/:id/accept", handler.Accept)
+		orders.POST("/:id/complete", handler.Complete)
+		orders.POST("/:id/cancel", handler.Cancel)
+	}
+
+	return r
+}
