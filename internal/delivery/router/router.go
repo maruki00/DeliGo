194474a1d8622e043bfab973fd458f3f770f02eb
Abdellah Oulahyane <aitbaha48@gmail.com>
2026-07-22 22:22:39 +0300

    Add router.go

---
+++ internal/delivery/router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"github.com/maruki00/deligo/delivery/handlers"
+	"github.com/maruki00/deligo/delivery/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(h *handlers.DeliveryHandler) *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(middleware.RequestLogger())
+
+	api := r.Group("/api/v1")
+	{
+		api.POST("/couriers", h.RegisterCourier)
+		api.POST("/tracking/ping", h.PingLocation)
+
+		orders := api.Group("/orders/:order_id")
+		{
+			orders.POST("/accept", h.AcceptOrder)
+			orders.POST("/arrive", h.ArriveAtRestaurant)
+			orders.POST("/pickup", h.StartDelivery)
+			orders.POST("/complete", h.CompleteDelivery)
+		}
+	}
+	return r
+}
